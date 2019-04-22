622bd11cd162d39eeed999b8bedd4b6b70987df9
Manon Jacquin <manon.jacquin@gmail.com>
2019-04-22 11:55:25 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// delete main id
+document.getElementById('main').remove()
+
+// "has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside"
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Manon is the champion!'
+// optional for css:
+newHeader.className = 'victory'
