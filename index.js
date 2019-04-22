6f690fca36861be2ff3c90a1284ec804aa6e5e28
keoki33 <gwong323@yahoo.com>
2019-04-22 15:36:37 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+main.remove()
+
+
+
+let newHeader = document.createElement('h1')
+
+document.head.appendChild(newHeader)
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = 'YOUR-NAME is the champion';
