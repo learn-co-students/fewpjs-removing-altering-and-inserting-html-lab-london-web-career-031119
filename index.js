6a5f33d0bf84cbae6b0effc4023940aeff2b4d05
oli-gra <oliver.grant@gmail.com>
2019-04-23 14:34:10 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main#main').remove()
+let newHeader = document.createElement("h1")
+newHeader.setAttribute('id','victory')
+newHeader.textContent = "I is the champion"
+document.appendChild(newHeader)
