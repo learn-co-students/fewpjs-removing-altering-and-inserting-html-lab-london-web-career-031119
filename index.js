dfcc11738a3c0803f0f4c5efc3692a425fd49f8a
Tola <you@example.com>
2019-04-24 14:57:25 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Tola is the champion';
+//newHeader.className = 'victory';
+document.body.append(newHeader)
